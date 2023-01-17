a73eadd4226636599d454d3556ade1e714412a7c
patriNunez <patrinunez02@gmail.com>
2023-01-17 02:07:40 +0100

    ci: angular material added

--- src/app/modules/heroes/heroes.module.ts
+++ src/app/modules/heroes/heroes.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [HeroesRoutingModule.components],
-  imports: [CommonModule, HeroesRoutingModule],
+  imports: [CommonModule, HeroesRoutingModule, MatSlideToggleModule],
 })
 export class HeroesModule {}
